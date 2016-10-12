a72c05ef059335d882d7dc74977887e274f7a98d
nathan <nathankou12@gmail.com>
2016-10-12 01:45:47 -0500

    worked on quote generator

--- quote_generator/quoteGen.js
+++ quote_generator/quoteGen.js
@@ -1,6 +1,14 @@
+/*
+	TODO: backgroudn pictures
+	no repeats
+	twitter button
+*/
+
+
 var quote = document.getElementById("quote");
 var author = document.getElementById("author");
 var newQuoteButton = document.getElementById("newQuoteButton");
+var background = document.getElementById("background");
 
 var quotes = [
 	{
@@ -12,10 +20,6 @@ var quotes = [
 		author: "Lenny Robinson AKA Route 29 Batman"
 	},
 	{
-		quote: "Gotta have opposites dark and light, light and dark in painting. It's like in life. Gotta have a little sadness once in a while so you know when the good times come. I'm waiting on the good times now.",
-		author: "Bob Ross"
-	},
-	{
 		quote: "Service to others is the rent you pay for your room here on Earth.",
 		author: "Muhammed Ali"
 	},
@@ -24,10 +28,42 @@ var quotes = [
 		author: "unknown"
 	},
 	{
-		quote: "I'd always end up broken down on the highway. When I stood there trying to flag someone down, nobody stopped. But when I pushed my own car, other drivers would get out and push with me. If you want help, help yourself - people like to see that.",
-		author: "Chris Rock"
+		quote: "Somtimes you will never know the value of a moment until it becomes a memory.",
+		author: "Dr. Seuss"
+	},
+	{
+		quote: "I like you just the way you are.",
+		author: "Mr. Rogers"
+	},
+	{
+		quote: "No such thign as spare time. No such thing as free time. No such thing as down time. All you got is life time. Go.",
+		author: "Henry Rollins"
+	},
+	{
+		quote: "The trouble is, you think you have time.",
+		author: "Buddha"
+	},
+	{
+		quote: "Talent is pursued interest.",
+		author: "Bob Ross"
+	},
+	{
+		quote: "Every day may not be good, but there's something good in every day.",
+		author: "chibird"
 	}
 ];
+
+var colors = [
+	"rgb(155, 89, 182)",
+	"rgb(119, 177, 169)",
+	"rgb(189, 187, 153)",
+	"rgb(52, 34, 36)",
+	"rgb(243, 156, 18)",
+	"rgb(71, 46, 50)",
+	"rgb(115, 168, 87)",
+	"rgb(22, 160, 133)",
+
+]
 
 generateQuote();
 
@@ -35,7 +71,13 @@ newQuoteButton.onclick = generateQuote;
 
 function generateQuote() {
 	var quoteIndex = Math.floor(Math.random() * quotes.length);
+	var colorIndex = Math.floor(Math.random() * colors.length);
 	quote.innerHTML = "\"" + quotes[quoteIndex].quote + "\"";
 	author.innerHTML = "-" + quotes[quoteIndex].author;
+	quote.style.color = colors[colorIndex];
+	author.style.color = colors[colorIndex];
+	background.setAttribute("style", "background-color:" + colors[colorIndex]);
+	newQuoteButton.setAttribute("style", "background-color:" + colors[colorIndex]);
+	
 }
 
